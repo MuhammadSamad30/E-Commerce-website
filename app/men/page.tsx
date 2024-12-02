1f1186e47068e6daac2bae6f0ee58a585a7db395
MuhammadSamad30 <romivlogs11@gmail.com>
2024-12-03 02:04:20 +0500

    e-commerce-website

--- app/men/page.tsx
+++ app/men/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const men = () => {
   return (
     <div>
-      <h2 className="text-xl md:text-3xl pt-7 pl-[40%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
+      <h2 className="text-xl md:text-3xl pt-7 md:pl-[40%] pl-[6%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
         Men&apos;s category
       </h2>
         <Mens/>
--- app/women/page.tsx
+++ app/women/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Women = () => {
   return (
     <div>
-      <h2 className="text-xl md:text-3xl pt-7 pl-[40%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
+      <h2 className="text-xl md:text-3xl pt-7 md:pl-[40%] pl-[6%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
         Women&apos;s category
       </h2>
       <Womens />
