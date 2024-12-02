99e5d8c25fe73d082cb4e7feadbd45ccce4a42c0
MuhammadSamad30 <romivlogs11@gmail.com>
2024-12-03 01:56:13 +0500

    e-commerce-website

--- app/makeup/page.tsx
+++ app/makeup/page.tsx
@@ -4,6 +4,9 @@ import React from 'react'
 const makeup = () => {
   return (
     <div>
+      <h2 className="text-xl md:text-3xl pt-7 pl-[44%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
+        Makeup&apos;s
+      </h2>
         <Makeups/>
     </div>
   )
--- app/men/page.tsx
+++ app/men/page.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 const men = () => {
   return (
     <div>
+      <h2 className="text-xl md:text-3xl pt-7 pl-[40%] font-bold mb-4 text-center md:text-left text-[#709ec9]">
+        Men&apos;s category
+      </h2>
         <Mens/>
         <ShoesMale/>
     </div>
